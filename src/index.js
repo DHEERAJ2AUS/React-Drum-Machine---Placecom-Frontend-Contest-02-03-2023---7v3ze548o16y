0fd0d96fdbadfeb1589218ce9ca34ba3f8594e7c
DHEERAJ2AUS <108796274+DHEERAJ2AUS@users.noreply.github.com>
2023-03-02 19:30:36 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,7 @@
 import React from "react";
-import Pad from "./Pad";
-import {bank1} from "./App"
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
 
-function Pads() {
-    const keypadCode = Object.keys(bank1);
 
-
-    const playSound = e => {
-    }
-
-
-    return (
-      <div id='div-pads'>
-        {keypadCode.map((pad, idx) => {
-          console.log(pad + idx)
-          return (
-            <Pad
-              id={pad+idx}
-              key={pad+idx}
-              handleClick={playSound}
-              element={pad} />
-          );
-        })}
-        <div id='display'> Show the name of current audio here </div>  
-      </div>
-    )
-  }
-
-  export default Pads ; 
+ReactDOM.render(<App />, document.getElementById("root"));
